a30f45b6f03abfa93fda1a5f2dd7e0b0b15fc6d7
PC-20210717BZNV\Administrator <qianjue7012@163.com>
2021-12-06 21:48:33 +0800

    fix(select组件):修复在小程序端出现的bug

--- uview-ui/components/u-select/u-select.vue
+++ uview-ui/components/u-select/u-select.vue
@@ -9,7 +9,16 @@
 				<u-icon name="arrow-down-fill" size="26" color="#c0c4cc"></u-icon>
 			</view>
 		</view> -->
-		<u-popup :maskCloseAble="maskCloseAble" mode="bottom" :popup="false" v-model="value" length="auto" :safeAreaInsetBottom="safeAreaInsetBottom" @close="close" :z-index="uZIndex">
+		<u-popup
+			:maskCloseAble="maskCloseAble"
+			mode="bottom"
+			:popup="false"
+			v-model="value"
+			length="auto"
+			:safeAreaInsetBottom="safeAreaInsetBottom"
+			@close="close"
+			:z-index="uZIndex"
+		>
 			<view class="u-select">
 				<view class="u-select__header" @touchmove.stop.prevent="">
 					<view
@@ -19,11 +28,9 @@
 						:hover-stay-time="150"
 						@tap="getResult('cancel')"
 					>
-						{{cancelText}}
+						{{ cancelText }}
 					</view>
-					<view class="u-select__header__title">
-						{{title}}
-					</view>
+					<view class="u-select__header__title">{{ title }}</view>
 					<view
 						class="u-select__header__confirm u-select__header__btn"
 						:style="{ color: moving ? cancelColor : confirmColor }"
@@ -32,7 +39,7 @@
 						@touchmove.stop=""
 						@tap.stop="getResult('confirm')"
 					>
-						{{confirmText}}
+						{{ confirmText }}
 					</view>
 				</view>
 				<view class="u-select__body">
@@ -50,27 +57,27 @@
 </template>
 
 <script>
-	/**
-	 * select 列选择器
-	 * @description 此选择器用于单列，多列，多列联动的选择场景。(从1.3.0版本起，不建议使用Picker组件的单列和多列模式，Select组件是专门为列选择而构造的组件，更简单易用。)
-	 * @tutorial http://uviewui.com/components/select.html
-	 * @property {String} mode 模式选择，"single-column"-单列模式，"mutil-column"-多列模式，"mutil-column-auto"-多列联动模式
-	 * @property {Array} list 列数据，数组形式，见官网说明
-	 * @property {Boolean} v-model 布尔值变量，用于控制选择器的弹出与收起
-	 * @property {Boolean} safe-area-inset-bottom 是否开启底部安全区适配(默认false)
-	 * @property {String} cancel-color 取消按钮的颜色（默认#606266）
-	 * @property {String} confirm-color 确认按钮的颜色(默认#2979ff)
-	 * @property {String} confirm-text 确认按钮的文字
-	 * @property {String} cancel-text 取消按钮的文字
-	 * @property {String} default-value 提供的默认选中的下标，见官网说明
-	 * @property {Boolean} mask-close-able 是否允许通过点击遮罩关闭Picker(默认true)
-	 * @property {String Number} z-index 弹出时的z-index值(默认10075)
-	 * @property {String} value-name 自定义list数据的value属性名 1.3.6
-	 * @property {String} label-name 自定义list数据的label属性名 1.3.6
-	 * @property {String} child-name 自定义list数据的children属性名，只对多列联动模式有效 1.3.7
-	 * @event {Function} confirm 点击确定按钮，返回当前选择的值
-	 * @example <u-select v-model="show" :list="list"></u-select>
-	 */
+/**
+ * select 列选择器
+ * @description 此选择器用于单列，多列，多列联动的选择场景。(从1.3.0版本起，不建议使用Picker组件的单列和多列模式，Select组件是专门为列选择而构造的组件，更简单易用。)
+ * @tutorial http://uviewui.com/components/select.html
+ * @property {String} mode 模式选择，"single-column"-单列模式，"mutil-column"-多列模式，"mutil-column-auto"-多列联动模式
+ * @property {Array} list 列数据，数组形式，见官网说明
+ * @property {Boolean} v-model 布尔值变量，用于控制选择器的弹出与收起
+ * @property {Boolean} safe-area-inset-bottom 是否开启底部安全区适配(默认false)
+ * @property {String} cancel-color 取消按钮的颜色（默认#606266）
+ * @property {String} confirm-color 确认按钮的颜色(默认#2979ff)
+ * @property {String} confirm-text 确认按钮的文字
+ * @property {String} cancel-text 取消按钮的文字
+ * @property {String} default-value 提供的默认选中的下标，见官网说明
+ * @property {Boolean} mask-close-able 是否允许通过点击遮罩关闭Picker(默认true)
+ * @property {String Number} z-index 弹出时的z-index值(默认10075)
+ * @property {String} value-name 自定义list数据的value属性名 1.3.6
+ * @property {String} label-name 自定义list数据的label属性名 1.3.6
+ * @property {String} child-name 自定义list数据的children属性名，只对多列联动模式有效 1.3.7
+ * @event {Function} confirm 点击确定按钮，返回当前选择的值
+ * @example <u-select v-model="show" :list="list"></u-select>
+ */
 
 export default {
 	props: {
@@ -179,15 +186,15 @@ export default {
 		value: {
 			immediate: true,
 			handler(val) {
-				if(val) setTimeout(() => this.init(), 10);
+				if (val) setTimeout(() => this.init(), 10);
 			}
-		},
+		}
 	},
 	computed: {
 		uZIndex() {
 			// 如果用户有传递z-index值，优先使用
 			return this.zIndex ? this.zIndex : this.$u.zIndex.popup;
-		},
+		}
 	},
 	methods: {
 		// 标识滑动开始，只有微信小程序才有这样的事件
@@ -217,17 +224,17 @@ export default {
 		// 计算列数
 		setColumnNum() {
 			// 单列的列数为1
-			if(this.mode == 'single-column') this.columnNum = 1;
+			if (this.mode == 'single-column') this.columnNum = 1;
 			// 多列时，this.list数组长度就是列数
-			else if(this.mode == 'mutil-column') this.columnNum = this.list.length;
+			else if (this.mode == 'mutil-column') this.columnNum = this.list.length;
 			// 多列联动时，通过历遍this.list的第一个元素，得出有多少列
-			else if(this.mode == 'mutil-column-auto') {
+			else if (this.mode == 'mutil-column-auto') {
 				let num = 1;
 				let column = this.list;
 				// 只要有元素并且第一个元素有children属性，继续历遍
-				while(column[0][this.childName]) {
+				while (column[0][this.childName]) {
 					column = column[0] ? column[0][this.childName] : {};
-					num ++;
+					num++;
 				}
 				this.columnNum = num;
 			}
@@ -236,7 +243,7 @@ export default {
 		setColumnData() {
 			let data = [];
 			this.selectValue = [];
-			if(this.mode == 'mutil-column-auto') {
+			if (this.mode == 'mutil-column-auto') {
 				// 获得所有数据中的第一个元素
 				let column = this.list[this.defaultSelector.length ? this.defaultSelector[0] : 0];
 				// 通过循环所有的列数，再根据设定列的数组，得出当前需要渲染的整个列数组
@@ -251,7 +258,7 @@ export default {
 						column = column[this.defaultSelector[i]][this.childName];
 					}
 				}
-			} else if(this.mode == 'single-column') {
+			} else if (this.mode == 'single-column') {
 				data[0] = this.list;
 			} else {
 				data = this.list;
@@ -261,15 +268,15 @@ export default {
 		// 获取默认选中的值，如果没有设置defaultValue，就默认选中每列的第一个
 		setSelectValue() {
 			let tmp = null;
-			for(let i = 0; i < this.columnNum; i++) {
+			for (let i = 0; i < this.columnNum; i++) {
 				tmp = this.columnData[i][this.defaultSelector[i]];
 				let data = {
 					value: tmp ? tmp[this.valueName] : null,
 					label: tmp ? tmp[this.labelName] : null
 				};
 				// 判断是否存在额外的参数，如果存在，就返回
-				if(tmp && tmp.extra) data.extra = tmp.extra;
-				this.selectValue.push(data)
+				if (tmp && tmp.extra) data.extra = tmp.extra;
+				this.selectValue.push(data);
 			}
 		},
 		// 列选项
@@ -278,12 +285,12 @@ export default {
 			let columnIndex = e.detail.value;
 			// 由于后面是需要push进数组的，所以需要先清空数组
 			this.selectValue = [];
-			if(this.mode == 'mutil-column-auto') {
+			this.defaultSelector = columnIndex;
+			if (this.mode == 'mutil-column-auto') {
 				// 对比前后两个数组，寻找变更的是哪一列，如果某一个元素不同，即可判定该列发生了变化
 				this.lastSelectIndex.map((val, idx) => {
 					if (val != columnIndex[idx]) index = idx;
 				});
-				this.defaultSelector = columnIndex;
 				for (let i = index + 1; i < this.columnNum; i++) {
 					// 当前变化列的下一列的数据，需要获取上一列的数据，同时需要指定是上一列的第几个的children，再往后的
 					// 默认是队列的第一个为默认选项
@@ -297,38 +304,37 @@ export default {
 					let data = this.columnData[index][columnIndex[index]];
 					let tmp = {
 						value: data ? data[this.valueName] : null,
-						label: data ? data[this.labelName] : null,
+						label: data ? data[this.labelName] : null
 					};
 					// 判断是否有需要额外携带的参数
-					if(data && data.extra !== undefined) tmp.extra = data.extra;
+					if (data && data.extra !== undefined) tmp.extra = data.extra;
 					this.selectValue.push(tmp);
-
-				})
+				});
 				// 保存这一次的结果，用于下次列发生变化时作比较
 				this.lastSelectIndex = columnIndex;
-			} else if(this.mode == 'single-column') {
+			} else if (this.mode == 'single-column') {
 				let data = this.columnData[0][columnIndex[0]];
 				// 初始默认选中值
 				let tmp = {
 					value: data ? data[this.valueName] : null,
-					label: data ? data[this.labelName] : null,
+					label: data ? data[this.labelName] : null
 				};
 				// 判断是否有需要额外携带的参数
-				if(data && data.extra !== undefined) tmp.extra = data.extra;
+				if (data && data.extra !== undefined) tmp.extra = data.extra;
 				this.selectValue.push(tmp);
-			} else if(this.mode == 'mutil-column') {
+			} else if (this.mode == 'mutil-column') {
 				// 初始默认选中值
 				columnIndex.map((item, index) => {
 					let data = this.columnData[index][columnIndex[index]];
 					// 初始默认选中值
 					let tmp = {
 						value: data ? data[this.valueName] : null,
-						label: data ? data[this.labelName] : null,
+						label: data ? data[this.labelName] : null
 					};
 					// 判断是否有需要额外携带的参数
-					if(data && data.extra !== undefined) tmp.extra = data.extra;
+					if (data && data.extra !== undefined) tmp.extra = data.extra;
 					this.selectValue.push(tmp);
-				})
+				});
 			}
 		},
 		close() {
@@ -350,10 +356,9 @@ export default {
 </script>
 
 <style scoped lang="scss">
-@import "../../libs/css/style.components.scss";
+@import '../../libs/css/style.components.scss';
 
 .u-select {
-
 	&__action {
 		position: relative;
 		line-height: $u-form-item-height;
@@ -363,7 +368,7 @@ export default {
 			position: absolute;
 			right: 20rpx;
 			top: 50%;
-			transition: transform .4s;
+			transition: transform 0.4s;
 			transform: translateY(-50%);
 			z-index: 1;
 
