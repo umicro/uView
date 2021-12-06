4d2498b53a9fe332abe98901e4a15def32748b10
PC-20210717BZNV\Administrator <qianjue7012@163.com>
2021-12-06 22:50:08 +0800

    fix(numberBox步进器)：修复设置positive-integer为false,允许输入小数时，在小程序真机上无法输入小数的bug

--- pages/componentsC/numberBox/index.vue
+++ pages/componentsC/numberBox/index.vue
@@ -3,14 +3,21 @@
 		<view class="u-demo-wrap">
 			<view class="u-demo-title">演示效果</view>
 			<view class="u-demo-area">
-				<u-number-box v-model="value" :bg-color="bgColor" :color="color" :min="0"
-				:step="step" :disabled="disabled" @change="change" @focus="focus"></u-number-box>
+				<u-number-box
+					v-model="value"
+					:bg-color="bgColor"
+					:color="color"
+					:min="0"
+					:step="step"
+					:disabled="disabled"
+					:positive-integer="positiveInteger"
+					@change="change"
+					@focus="focus"
+				></u-number-box>
 			</view>
 		</view>
 		<view class="u-config-wrap">
-			<view class="u-config-title u-border-bottom">
-				参数配置
-			</view>
+			<view class="u-config-title u-border-bottom">参数配置</view>
 			<view class="u-config-item">
 				<view class="u-item-title">初始值</view>
 				<u-subsection :list="[1, 5, 18]" @change="valueChange"></u-subsection>
@@ -23,6 +30,10 @@
 				<view class="u-item-title">是否禁用</view>
 				<u-subsection current="1" :list="['是', '否']" @change="disabledChange"></u-subsection>
 			</view>
+			<!-- <view class="u-config-item">
+				<view class="u-item-title">是否允许输入小数</view>
+				<u-subsection current="1" :list="['是', '否']" @change="inputTypeChange"></u-subsection>
+			</view> -->
 			<view class="u-config-item">
 				<view class="u-item-title">步进值</view>
 				<u-subsection :list="[1, 3, 5, 8]" @change="stepChange"></u-subsection>
@@ -36,11 +47,11 @@ export default {
 	data() {
 		return {
 			value: 1,
-			bgColor: "#F2F3F5",
+			bgColor: '#F2F3F5',
 			color: '#323233',
 			disabled: false,
 			step: 1,
-			
+			positiveInteger: true
 		};
 	},
 	methods: {
@@ -48,11 +59,11 @@ export default {
 			this.value = index == 0 ? 1 : index == 1 ? 5 : 18;
 		},
 		styleChange(index) {
-			if(index == 0) {
+			if (index == 0) {
 				this.bgColor = '#ff6d00';
 				this.color = '#fff';
 			} else {
-				this.bgColor = "#F2F3F5";
+				this.bgColor = '#F2F3F5';
 				this.color = '#323233';
 			}
 		},
@@ -67,11 +78,16 @@ export default {
 		},
 		focus() {
 			console.log('focus');
+		},
+		inputTypeChange(index) {
+			this.value = 1;
+			this.positiveInteger = index == 1 ? true : false;
 		}
 	}
 };
 </script>
 
 <style lang="scss" scoped>
-.u-demo {}
+.u-demo {
+}
 </style>
